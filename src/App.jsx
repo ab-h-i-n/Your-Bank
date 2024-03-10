409671c9fc8387e7ef6b1728621fd131ffaccd31
ab-h-i-n <abhinshelly@gmail.com>
2024-03-10 07:51:24 +0530

    user context

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer";
 import {UserContext} from "./User";
+import { useContext, useState } from "react";
+
 
 function App() {
 
-  const user = "user";
+  const userData = useContext(UserContext);
 
   return (
     <div className="App lexend ">
-      <UserContext.Provider value={user}>
+      <UserContext.Provider value={userData}>
         <BrowserRouter>
           <Header />
           <Routes>
